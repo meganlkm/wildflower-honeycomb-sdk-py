60e718ce2ee880ba1206a89114435754105713ad
Megan <megan.lkm@gmail.com>
2019-08-22 09:49:10 -0500

    options for file name and path

--- cli/__init__.py
+++ cli/__init__.py
@@ -39,8 +39,10 @@ def datapoint(ctx):
 
 @datapoint.command('get')
 @click.argument('data_id')
+@click.option('--file-path', help="Save the downloaded file in this path. Default is the current working directory.")
+@click.option('--file-name', help="Save the file as this. Default is the last part of the key being downloaded.")
 @click.pass_context
-def datapoint_get(ctx, data_id):
+def datapoint_get(ctx, data_id, file_path, file_name):
     """get datapoint"""
     query = """query getDP($data_id: ID!) {
         getDatapoint(data_id: $data_id) {
@@ -53,11 +55,20 @@ def datapoint_get(ctx, data_id):
     try:
         bucket = response['getDatapoint']['file']['bucketName']
         key = response['getDatapoint']['file']['key']
-        print('Downloading: {}'.format(os.path.join(bucket, key)))
-        s3 = boto3.resource('s3')
-        file_path = os.path.join(os.getcwd(), '{}-{}'.format(data_id, key.split('/').pop()))
-        s3.meta.client.download_file(bucket, key, file_path)
-        print('File saved: {}'.format(file_path))
     except TypeError:
         # raise Exception('Datapoint not found.')
-        click.echo('Datapoint not found.', err=True)
+        return click.echo('Datapoint not found.', err=True)
+
+    click.echo('Downloading: {}'.format(os.path.join(bucket, key)))
+    if not file_path:
+        file_path = os.getcwd()
+    if not file_name:
+        file_name = '{}-{}'.format(data_id, key.split('/').pop())
+    file_ext = key.split('.').pop()
+    if not file_name.endswith(file_ext):
+        file_name += '.{}'.format(file_ext)
+    save_path = os.path.join(file_path, file_name)
+
+    s3 = boto3.resource('s3')
+    s3.meta.client.download_file(bucket, key, save_path)
+    return click.echo('File saved: {}'.format(save_path))
