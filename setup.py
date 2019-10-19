046a399ae968fdf92c0436017e4377cadd181689
Theodore Quinn <tcquinn@tcquinn.org>
2019-10-19 11:58:03 -0500

    Use consistent syntax and structure for dependencies in setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,17 @@ from setuptools import setup, find_packages
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 VERSION = open(os.path.join(BASEDIR, 'VERSION')).read().strip()
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(BASEDIR))
+BASE_DEPENDENCIES = [
+    'wf-gqlpycgen>=0.5.7',
+    'requests>=2.21',
+    'Jinja2>=2.10',
+    'gql>=0.1.0',
+    'PyYAML>=3.13',
+    'click>=6.7',
+    'boto3>=1.9.213'
+]
 
-test_dependencies = [
+TEST_DEPENDENCIES = [
     'pytest==3.0.6',
     'pytest-cov==2.4.0',
     'pytest-mock==1.5.0',
@@ -15,10 +22,13 @@ test_dependencies = [
     'httpretty==0.8.14'
 ]
 
-local_dependencies = [
+LOCAL_DEPENDENCIES = [
     'tox==2.6.0',
     'tox-pyenv==1.0.3'
 ]
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(BASEDIR))
 
 setup(
     name='wildflower-honeycomb-sdk',
@@ -30,19 +40,11 @@ setup(
     url='https://github.com/Wildflowerschools/py-honeycomb-sdk',
     author='optimuspaul',
     author_email='paul.decoursey@wildflowerschools.org',
-    install_requires= [
-        'wf-gqlpycgen>=0.5.7',
-        'requests>=2.21',
-        'Jinja2>=2.10',
-        'gql>=0.1.0',
-        'PyYAML>=3.13',
-        'click>=6.7',
-        'boto3>=1.9.213'
-    ],
-    tests_require = test_dependencies,
+    install_requires= BASE_DEPENDENCIES,
+    tests_require = TEST_DEPENDENCIES,
     extra_require = {
-        'test': test_dependencies,
-        'local': local_dependencies
+        'test': TEST_DEPENDENCIES,
+        'local': LOCAL_DEPENDENCIES
     },
     entry_points={
         'console_scripts': [
